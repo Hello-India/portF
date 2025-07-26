b22a092ce72d77a05460b07eb30e63d9ce8669f5
Hello-India <119911658+Hello-India@users.noreply.github.com>
2025-07-26 11:01:51 +0530

    Create dragon.js

---
+++ MyPORT/dragon.js
@@ -0,0 +1,32 @@
+const cursor = document.querySelector('.cursor');
+const outer = document.querySelector('.outer');
+const inner = document.querySelector('.inner');
+
+document.addEventListener('mousemove', (e) => {
+  const x = e.clientX, y = e.clientY;
+
+  cursor.style.top = y + "px";
+  cursor.style.left = x + "px";
+
+  outer.style.top = y + "px";
+  outer.style.left = x + "px";
+
+  inner.style.top = y + "px";
+  inner.style.left = x + "px";
+});
+
+document.addEventListener('mousedown', () => {
+  cursor.classList.add("pointer");
+});
+
+document.addEventListener('mouseup', () => {
+  cursor.classList.remove("pointer");
+});
+
+document.addEventListener('mouseleave', () => {
+  cursor.style.display = 'none';
+});
+
+document.addEventListener('mouseenter', () => {
+  cursor.style.display = 'block';
+});
